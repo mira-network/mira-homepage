c9d5cd9f87a20ce82545510914ae56a152205ed5
Magnus Bratt <magnus.bratt@mira.se>
2022-07-18 01:46:41 +0200

    Gör slider för logos responsiv

--- _site/_assets/parcel.js
+++ _site/_assets/parcel.js
@@ -455,7 +455,28 @@ require("./_bootstrap.js");
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000,
-      dots: true
+      dots: true,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      }, {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }, {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
     });
   });
 })();
